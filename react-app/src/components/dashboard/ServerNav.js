ea9d5fe909b0fb453d061cc28b24802f576cee24
Brian <brianshkim25@gmail.com>
2022-07-12 11:25:48 -0700

    some fixes

--- react-app/src/components/dashboard/ServerNav.js
+++ react-app/src/components/dashboard/ServerNav.js
@@ -80,7 +80,7 @@ const ServerNav = () => {
                     {servers && servers.list && servers.list.map((Server) => (
 
 
-                        <li key={Server.id}><button id={`button-${Server.id}`}
+                        <li className="serverlist-list" key={Server.id}><button id={`button-${Server.id}`}
                         onClick={(e)=>gotoserver(e, Server.id)}
                         className="serverbuttons"
                         onContextMenu={(e) => rightonclick(e, Server.id)} > {serverId === Server.id && <Menu x={x} y={y} serverid={Server.id} display={display} />}{Server.name[0].toUpperCase()} </button></li>
