10f547511133d9b9ffbebb41336ffce61ce9b40f
Brian <brianshkim25@gmail.com>
2022-08-02 12:50:09 -0700

    css voice channels

--- react-app/src/components/UsersList.js
+++ react-app/src/components/UsersList.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux"
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { io } from "socket.io-client"
 import './onlinestatus.css'
 import DiscordLogoWhite from './SplashPage/DiscordLogoWhite.png'
 let socket;
 
-function UsersList(){
-  const {serverid} = useParams()
+function UsersList() {
+  const { serverid } = useParams()
   const dispatch = useDispatch()
   const [users, setUsers] = useState([]);
   const user = useSelector(state => state.session.user)
   const [usersOnline, setUsersOnline] = useState([])
-  const servers = useSelector(state=> state.servers?.list)
+  const servers = useSelector(state => state.servers?.list)
 
-  let server = servers?.filter(server=>server.id == serverid)
+  let server = servers?.filter(server => server.id == serverid)
 
 
   useEffect(() => {
@@ -31,7 +31,7 @@ function UsersList(){
 
     socket.connect()
 
-    socket.on("disconnection", (users)=>{
+    socket.on("disconnection", (users) => {
       const newusers = [...users.users]
 
       setUsersOnline(newusers)
@@ -50,7 +50,7 @@ function UsersList(){
 
     socket.emit("connection", ({ userId: user.id }))
 
-    return (()=>{
+    return (() => {
       socket.disconnect()
     })
 
@@ -67,40 +67,40 @@ function UsersList(){
 
 
 
-if(serverid){
+  if (serverid) {
 
-  return (
-    <div className="UserListContainer">
-      <p className="userlistonline">ONLINE - {usersOnline.length} </p>
-      <ul>
-        {!!servers && servers.length>0 && server && server.length >0 && server[0].users.filter(user=>usersOnline.includes(user.id)).map((user) => (
-
-
-          <li className="userslist" key={user.id}>
-          <div className="usercontainer" ><div className="useravatar"><img className="discordavatar" src={DiscordLogoWhite} height="18" width="18"></img> {usersOnline.includes(user.id) ? <div className="onlinestatusshape"></div>:null}</div> </div>
-          <span className="usernametext">{user.username}</span>
-          </li>
-
-    ))}
-    </ul>
-    <p className="userlistoffline">OFFLINE - {servers && servers.length>0 && server && server.length >0 && server[0].users.length - usersOnline.length}</p>
-    <ul>
-        {!!servers && servers.length>0 && server && server.length >0 && server[0].users.filter(user=>!usersOnline.includes(user.id)).map((user) => (
+    return (
+      <div className="UserListContainer">
+        <p className="userlistonline">ONLINE - {usersOnline.length} </p>
+        <ul>
+          {!!servers && servers.length > 0 && server && server.length > 0 && server[0].users.filter(user => usersOnline.includes(user.id)).map((user) => (
 
 
-          <li className="userslist" key={user.id}>
-          <div className="usercontainer" ><div className="useravatar"><img className="discordavatar" src={DiscordLogoWhite} height="18" width="18"></img> {usersOnline.includes(user.id) ? <div className="onlinestatusshape"></div>:null}</div> </div>
-          <span className="usernametext">{user.username}</span>
-          </li>
+            <li className="userslist" key={user.id}>
+              <div className="usercontainer" ><div className="useravatar"><img className="discordavatar" src={DiscordLogoWhite} height="18" width="18"></img> {usersOnline.includes(user.id) ? <div className="onlinestatusshape"></div> : null}</div> </div>
+              <span className="usernametext">{user.username}</span>
+            </li>
 
-    ))}
-    </ul>
-    </div>
-  );
-}
-else{
-  return(null)
-}
+          ))}
+        </ul>
+        <p className="userlistonline">OFFLINE - {servers && servers.length > 0 && server && server.length > 0 && server[0].users.length - usersOnline.length}</p>
+        <ul className="usersoffline">
+          {!!servers && servers.length > 0 && server && server.length > 0 && server[0].users.filter(user => !usersOnline.includes(user.id)).map((user) => (
+
+
+            <li className="userslist2" key={user.id}>
+              <div className="usercontainer" ><div className="useravatar"><img className="discordavatar" src={DiscordLogoWhite} height="18" width="18"></img> {usersOnline.includes(user.id) ? <div className="onlinestatusshape"></div> : null}</div> </div>
+              <span className="usernametext">{user.username}</span>
+            </li>
+
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  else {
+    return (null)
+  }
 }
 
 export default UsersList;
