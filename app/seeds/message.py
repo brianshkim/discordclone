416f618a11b84f08467d181b7156240c1179e18e
Brian <brianshkim25@gmail.com>
2022-08-01 20:18:40 -0700

    no message seeds

--- app/seeds/message.py
+++ app/seeds/message.py
@@ -12,17 +12,7 @@ mstimetwo = date_time_milliseconds(datetime.datetime.utcnow())
 
 # Adds a demo user, you can add other users here if you want
 def seed_messages():
-    content1 = Message(
-        content='hello',channelId=1, userId="1", username="demo", createdate=mstimetwo)
-    content2 = Message(
-        content='my name is demo', channelId=1, userId="1", username="demo", createdate=mstimetwo)
-
-
-    db.session.add(content1)
-    db.session.add(content2)
-
-
-    db.session.commit()
+    pass
 
 def undo_messages():
     db.session.execute('TRUNCATE messages RESTART IDENTITY CASCADE;')
